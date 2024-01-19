7f812575a4f5dc9c90e302d725dee3529c9c37fa
lanskikh <lanskikh-mikhail@mail.ru>
2024-01-20 00:18:15 +0500

    expo envs

--- src/app-flat/config/api/index.ts
+++ src/app-flat/config/api/index.ts
@@ -1,2 +1,2 @@
 // export const API_URL = `${process.env.APP_URL}`
-export const API_SERVER_URL = `${process.env.REACT_APP_SERVER_URL}`
+export const API_SERVER_URL = `${process.env.EXPO_PUBLIC_SERVER_URL}`
